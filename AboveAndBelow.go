f1dba3b7c75f1fc99f5926e6fc98858e6227aff2
Apoxtrophe <cyggardner@gmail.com>
2023-06-26 18:30:03 -0600

    DensitySwap

--- AboveAndBelow.go
+++ AboveAndBelow.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	//"image/color"
-
 	"github.com/hajimehoshi/ebiten/v2"
 	"golang.org/x/image/colornames"
-
 	//"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"fmt"
 	"image/color"
@@ -29,8 +27,8 @@ func main() {
 
 type Game struct {
 	Pixels       []byte
-	FirstBuffer  [][]int
-	SecondBuffer [][]int
+	Ichi  [][]int
+	Ni [][]int
 	Index        int
 	PixelSize    int
 	//ebiten.Game
@@ -40,11 +38,11 @@ func NewGame() *Game {
 	g := &Game{}
 	g.PixelSize = 10
 	g.Pixels = make([]byte, screenWidth*screenHeight*4)
-	g.FirstBuffer = make([][]int, screenHeight/g.PixelSize)
-	g.SecondBuffer = make([][]int, screenHeight/g.PixelSize)
-	for i := range g.FirstBuffer {
-		g.FirstBuffer[i] = make([]int, screenWidth/g.PixelSize)
-		g.SecondBuffer[i] = make([]int, screenWidth/g.PixelSize)
+	g.Ichi = make([][]int, screenHeight/g.PixelSize)
+	g.Ni = make([][]int, screenHeight/g.PixelSize)
+	for i := range g.Ichi {
+		g.Ichi[i] = make([]int, screenWidth/g.PixelSize)
+		g.Ni[i] = make([]int, screenWidth/g.PixelSize)
 	}
 	return g
 }
@@ -60,9 +58,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	for row := 0; row < len(g.FirstBuffer); row++ {
-		for col := 0; col < len(g.FirstBuffer[row]); col++ {
-			clr := ElementMap[g.FirstBuffer[row][col]].Color
+	for row := 0; row < len(g.Ichi); row++ {
+		for col := 0; col < len(g.Ichi[row]); col++ {
+			clr := ElementMap[g.Ichi[row][col]].Color
 			for y := 0; y < g.PixelSize; y++ {
 				for x := 0; x < g.PixelSize; x++ {
 					i := ((row*g.PixelSize+y)*screenWidth + (col*g.PixelSize + x)) * 4
@@ -92,18 +90,18 @@ func MouseInteract(g *Game) {
 	fmt.Println(g.Index)
 	//Clicking detection
 	if mouse_one {
-		g.FirstBuffer[world_y][world_x] = g.Index
+		g.Ichi[world_y][world_x] = g.Index
 	}
 	if mouse_two {
-		g.FirstBuffer[world_y][world_x] = 0
+		g.Ichi[world_y][world_x] = 0
 	}
 }
 
 func (g *Game) AliveArray() {
 	aliveCells := make([][2]int, 0)
-	for row := 0; row < len(g.FirstBuffer); row++ {
-		for col := 0; col < len(g.FirstBuffer[row]); col++ {
-			if g.FirstBuffer[row][col] != 0 {
+	for row := 0; row < len(g.Ichi); row++ {
+		for col := 0; col < len(g.Ichi[row]); col++ {
+			if g.Ichi[row][col] != 0 {
 				aliveCells = append(aliveCells, [2]int{row, col})
 			}
 		}
@@ -112,15 +110,15 @@ func (g *Game) AliveArray() {
 		aliveCells[i], aliveCells[j] = aliveCells[j], aliveCells[i]
 	})
 	// Reset next buffer
-	for row := range g.FirstBuffer {
-		for col := range g.FirstBuffer[row] {
-			g.SecondBuffer[row][col] = 0
+	for row := range g.Ichi {
+		for col := range g.Ichi[row] {
+			g.Ni[row][col] = 0
 		}
 	}
 
 	for _, pos := range aliveCells {
 		row, col := pos[0], pos[1]
-		switch g.FirstBuffer[row][col] {
+		switch g.Ichi[row][col] {
 		case 6:
 			g.Phys_Powder(row, col)
 		case 14:
@@ -131,14 +129,14 @@ func (g *Game) AliveArray() {
 
 		}
 	}
-	g.FirstBuffer, g.SecondBuffer = g.SecondBuffer, g.FirstBuffer
+	g.Ichi, g.Ni = g.Ni, g.Ichi
 }
 
 var ElementMap = map[int]Element{
 	0: {
 		Color:   colornames.Black,
 		Name:    "Void",
-		Density: 0,
+		Density: 100,
 	},
 	6: {
 		Color:   colornames.Gray,
@@ -152,7 +150,7 @@ var ElementMap = map[int]Element{
 	},
 	22: {
 		Color:   colornames.Cornflowerblue,
-		Name:    "white",
+		Name:    "Titanium",
 		Density: 45,
 	},
 }
@@ -163,8 +161,9 @@ type Element struct {
 	Density int
 }
 
-func (g *Game) FreeSpace(row, col int) bool {
-	if g.FirstBuffer[row][col] == 0 && g.SecondBuffer[row][col] == 0 {
+func (g *Game) IsFree(row, col int) bool {
+	// Check if free space is available in both buffers
+	if g.Ichi[row][col] == 0 && g.Ni[row][col] == 0 {
 		return true
 	}
 	return false
@@ -172,49 +171,45 @@ func (g *Game) FreeSpace(row, col int) bool {
 
 func (g *Game) Phys_Solid(row, col int) {
 	if col > 0 {
-		g.SecondBuffer[row][col] = g.FirstBuffer[row][col]
+		g.Ni[row][col] = g.Ichi[row][col]
 	}
 }
 
-func (g *Game) DensityCompare(row, col int) bool {
-	if row > 0 && g.FirstBuffer[row][col] != 0 && g.FirstBuffer[row-1][col] != 0 && ElementMap[g.FirstBuffer[row][col]].Density > ElementMap[g.FirstBuffer[row-1][col]].Density {
-		return true
-	}
-	return false
+func (g *Game) IsDenser (row, col int) bool {
+	return ElementMap[g.Ichi[row][col]].Density > ElementMap[g.Ichi[row + 1][col]].Density 
 }
-
-func (g *Game) DensitySwap(row, col int) {
-
-}
-
 func (g *Game) Phys_Powder(row, col int) {
-
-	if row+1 < len(g.FirstBuffer) && g.FreeSpace(row+1, col) {
-		g.SecondBuffer[row+1][col] = g.FirstBuffer[row][col]
-		g.SecondBuffer[row][col] = 0
+	// Fall down -> -> Swap densities -> fall either side -> fall left -> fall right -> stay stationary 
+	if row+1 < len(g.Ichi) && g.IsFree(row+1, col) {
+		g.Ni[row+1][col] = g.Ichi[row][col]
+		g.Ni[row][col] = 0
+	} else if g.IsDenser(row, col) {
+        Above := g.Ichi[row][col]
+        Below := g.Ichi[row+1][col]
+        g.Ni[row+1][col] = Above
+        if g.Ni[row][col] == 0 {
+            g.Ni[row][col] = Below
+        }
 	} else {
-		// If space below is not free, then check if diagonal spaces are free
-		// If both diagonal spaces are free, choose a random direction
-		// If one is free, move in that direction
-		// If none are free, stay in place
-		leftFree := col-1 >= 0 && g.FreeSpace(row+1, col-1)
-		rightFree := col+1 < len(g.FirstBuffer[row]) && g.FreeSpace(row+1, col+1)
+		
+		leftFree := col-1 >= 0 && g.IsFree(row+1, col-1)
+		rightFree := col+1 < len(g.Ichi[row]) && g.IsFree(row+1, col+1)
 
 		if leftFree && rightFree {
 			if rand.Float32() < 0.5 {
-				g.SecondBuffer[row+1][col-1] = g.FirstBuffer[row][col]
+				g.Ni[row+1][col-1] = g.Ichi[row][col]
 			} else {
-				g.SecondBuffer[row+1][col+1] = g.FirstBuffer[row][col]
+				g.Ni[row+1][col+1] = g.Ichi[row][col]
 			}
-			g.SecondBuffer[row][col] = 0
+			g.Ni[row][col] = 0
 		} else if leftFree {
-			g.SecondBuffer[row+1][col-1] = g.FirstBuffer[row][col]
-			g.SecondBuffer[row][col] = 0
+			g.Ni[row+1][col-1] = g.Ichi[row][col]
+			g.Ni[row][col] = 0
 		} else if rightFree {
-			g.SecondBuffer[row+1][col+1] = g.FirstBuffer[row][col]
-			g.SecondBuffer[row][col] = 0
+			g.Ni[row+1][col+1] = g.Ichi[row][col]
+			g.Ni[row][col] = 0
 		} else {
-			g.SecondBuffer[row][col] = g.FirstBuffer[row][col]
+			g.Ni[row][col] = g.Ichi[row][col]
 		}
 	}
 }
